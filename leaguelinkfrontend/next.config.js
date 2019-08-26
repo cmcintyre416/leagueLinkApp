dc2681649529b28409c379c2b97ece79ede52d30
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-08-26 19:26:30 -0400

    Added structure

--- leaguelinkfrontend/next.config.js
+++ leaguelinkfrontend/next.config.js
@@ -1,4 +1,12 @@
-const withSass = require('@zeit/next-sass')
+const withSass = require('@zeit/next-sass');
 module.exports = withSass({
-  cssModules: true
-})
+  cssModules: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
+});
--- leaguelinkfrontend/pages/index.js
+++ leaguelinkfrontend/pages/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Head from 'next/head';
-import css from '../index.scss';
+import GoogleButton from '../components/googleButton';
+import css from '../styles/index.scss';
 
 const Home = () => (
   <div className={css.homeWrapper}>
     <Head>
       <title>Home</title>
     </Head>
-    <div className={css.homeBlock}>
-      <img className={css.homeImage} src="../static/images/League-Link.png"/>
+    <div className={css.homeHerobanner}>
+      <div className={css.logo}>
+        <img className={css.logoImage} src="../static/images/roundedLogo.svg" aria-hidden="true" focusable="false"/>
+        <span className={css.logoTextTop}>eague</span>
+        <span className={css.logoTextBottom}>link</span>
+      </div>
       <h1 className={css.homeTitle}>Introducing League Link</h1>
       <p className={css.homeDescription}>
-        League Link is a multi purpose app for creating, managing and joining sports leagues and tournaments. From fun 'beer leagues' to leagues for 'hardos' we will be there to help you with the logistics. We are currently in the development process, however, please sign up here to receive updates and for a chance to be a first user.
+        League Link is a multi purpose app for creating, managing and joining sports leagues and tournaments. From fun 'beer leagues' to leagues for 'hardos' we will be there to help you with the logistics.
       </p>
+    </div>
+    <div className={css.homeBlock}>
+      <p className={css.homeBlockDescription}>
+        We are working hard to bring the app to life. please sign up here to receive updates and for a chance to be a first user.
+      </p>
+      <GoogleButton/>
     </div>
   </div>
 );
