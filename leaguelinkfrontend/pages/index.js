0285aa795835d8f763bf37294c8aaf1c1641e2d1
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-08-27 17:36:40 -0400

    modified pose effect

--- leaguelinkfrontend/pages/index.js
+++ leaguelinkfrontend/pages/index.js
@@ -10,6 +10,8 @@ const Box = posed.div({
     y: 0,
     opacity: 1,
     delay: 300,
+    delayChildren: 200,
+    staggerChildren: 50,
     transition: {
       y: { type: 'spring', stiffness: 500, damping: 50 },
       default: { duration: 1000 }
@@ -37,33 +39,35 @@ function Home() {
   }),[];
 
     return (
-    <PoseGroup>
-      {boxVisible && [
-      <Box key="box" className={`${css.homeWrapper} ${css.homeBox}`}>
-        <Head>
-          <title>League Link</title>
-          <link rel="shortcut icon" href="../static/images/favicon.png" />
-        </Head>
-        <div className={css.homeHerobanner}>
-          <Logo/>
-          <h1 className={css.homeTitle}>Introducing League Link</h1>
-          <p className={css.homeDescription}>
-            League Link is a multi purpose app for creating, managing and joining sports leagues and tournaments. From fun 'beer leagues' to leagues for 'hardos' we will be there to help you with the logistics.
-          </p>
-        </div>
-        <Square className={css.homeBlock} 
-            pose={squareHover ? "hovered" : "idle"}
-            onMouseEnter={() => setSquareHover(true)}
-            onMouseLeave={() => setSquareHover(false)}
-          >
-          <p className={css.homeBlockDescription}>
-            We are working hard to bring the app to life. please sign up here to receive updates and for a chance to be a first user.
-          </p>
-          <GoogleButton/>
-        </Square>
-      </Box>
-      ]}
-    </PoseGroup>
+    <>
+      <Head>
+        <title>League Link</title>
+        <link rel="shortcut icon" href="../static/images/favicon.png" />
+      </Head>
+      <PoseGroup>
+        {boxVisible && [
+        <Box key="box" className={`${css.homeWrapper} ${css.homeBox}`}>
+          <div className={css.homeHerobanner}>
+            <Logo/>
+            <h1 className={css.homeTitle}>Introducing League Link</h1>
+            <p className={css.homeDescription}>
+              League Link is a multi purpose app for creating, managing and joining sports leagues and tournaments. From fun 'beer leagues' to leagues for 'hardos' we will be there to help you with the logistics.
+            </p>
+          </div>
+          <Square className={css.homeBlock} 
+              pose={squareHover ? "hovered" : "idle"}
+              onMouseEnter={() => setSquareHover(true)}
+              onMouseLeave={() => setSquareHover(false)}
+            >
+            <p className={css.homeBlockDescription}>
+              We are working hard to bring the app to life. please sign up here to receive updates and for a chance to be a first user.
+            </p>
+            <GoogleButton/>
+          </Square>
+        </Box>
+        ]}
+      </PoseGroup>
+    </>
     );
 };
 
