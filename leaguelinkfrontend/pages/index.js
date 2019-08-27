b8c3b9e64a99397a8ca9550982309a296109dd71
Colin McIntyre <35696965+cmcintyre416@users.noreply.github.com>
2019-08-27 17:32:04 -0400

    modified pose effect

--- leaguelinkfrontend/pages/index.js
+++ leaguelinkfrontend/pages/index.js
@@ -3,11 +3,23 @@ import Head from 'next/head';
 import GoogleButton from '../components/googleButton';
 import Logo from '../components/logo';
 import css from '../styles/index.scss';
-import posed from 'react-pose';
+import posed, { PoseGroup } from 'react-pose';
 
 const Box = posed.div({
-  hidden: { opacity: 0 },
-  visible: { opacity: 1 }
+  enter: {
+    y: 0,
+    opacity: 1,
+    delay: 300,
+    transition: {
+      y: { type: 'spring', stiffness: 500, damping: 50 },
+      default: { duration: 1000 }
+    }
+  },
+  exit: {
+    y: 50,
+    opacity: 0,
+    transition: { duration: 100 }
+  }
 });
 
 const Square = posed.div({
@@ -17,39 +29,41 @@ const Square = posed.div({
 
 
 function Home() {
-  const [visible, setVisible] = useState(false);
+  const [boxVisible, setBoxVisible] = useState(false);
   const [squareHover, setSquareHover ] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      setVisible(true);
-    }, 500);
+      setBoxVisible(true);
   }),[];
 
     return (
-    <Box className={`${css.homeWrapper} ${css.homeBox}`} pose={visible ? 'visible' : 'hidden'}>
-      <Head>
-        <title>League Link</title>
-        <link rel="shortcut icon" href="../static/images/favicon.png" />
-      </Head>
-      <div className={css.homeHerobanner}>
-        <Logo/>
-        <h1 className={css.homeTitle}>Introducing League Link</h1>
-        <p className={css.homeDescription}>
-          League Link is a multi purpose app for creating, managing and joining sports leagues and tournaments. From fun 'beer leagues' to leagues for 'hardos' we will be there to help you with the logistics.
-        </p>
-      </div>
-      <Square className={css.homeBlock} 
-          pose={squareHover ? "hovered" : "idle"}
-          onMouseEnter={() => setSquareHover(true)}
-          onMouseLeave={() => setSquareHover(false)}
-        >
-        <p className={css.homeBlockDescription}>
-          We are working hard to bring the app to life. please sign up here to receive updates and for a chance to be a first user.
-        </p>
-        <GoogleButton/>
-      </Square>
-    </Box>
+    <PoseGroup>
+      {boxVisible && [
+      <Box key="box" className={`${css.homeWrapper} ${css.homeBox}`}>
+        <Head>
+          <title>League Link</title>
+          <link rel="shortcut icon" href="../static/images/favicon.png" />
+        </Head>
+        <div className={css.homeHerobanner}>
+          <Logo/>
+          <h1 className={css.homeTitle}>Introducing League Link</h1>
+          <p className={css.homeDescription}>
+            League Link is a multi purpose app for creating, managing and joining sports leagues and tournaments. From fun 'beer leagues' to leagues for 'hardos' we will be there to help you with the logistics.
+          </p>
+        </div>
+        <Square className={css.homeBlock} 
+            pose={squareHover ? "hovered" : "idle"}
+            onMouseEnter={() => setSquareHover(true)}
+            onMouseLeave={() => setSquareHover(false)}
+          >
+          <p className={css.homeBlockDescription}>
+            We are working hard to bring the app to life. please sign up here to receive updates and for a chance to be a first user.
+          </p>
+          <GoogleButton/>
+        </Square>
+      </Box>
+      ]}
+    </PoseGroup>
     );
 };
 
